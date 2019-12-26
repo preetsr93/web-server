79a8dd942dac9d9421912631acf313f0aae07dbd
Preethi Sri <preetsr93@gmail.com>
2019-12-26 18:34:37 +0530

    Initialise repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('private route hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log('Request on ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function (){
+    console.log ('Express server started on port '+ port);
+});
